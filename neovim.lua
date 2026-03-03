36b17acd119d9141183c3d4fa85617ba8701da4d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-03-03 13:39:24 +0900

    neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,8 +1,8 @@
 local prefix = "/opt/neovim"
 prepend_path("PATH", prefix .. "/bin")
 prepend_path("MANPATH", prefix .. "/share/man")
-set_alias("vi", "nvim")
-set_alias("view", "nvim -R")
+set_alias("vi", "nvim -u NORC")
+set_alias("view", "nvim -u NORC -R")
 set_alias("vimdiff", 'nvim -d -c "set noautoread"')
 pushenv("EDITOR", "nvim")
 pushenv("SUDO_EDITOR", "nvim --clean")
